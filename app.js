4a08095d3e94f23a5f9ffbc26656998d0102efc3
julio gabriel <juliogabriel78956@gmail.com>
2025-11-21 19:11:50 -0300

    Bootcamp

---
+++ app.js
@@ -0,0 +1,18 @@
+//  VARIAVEIS
+
+
+// console.log('Digite o nome do seu jogador')
+
+// let nickname = "VOSKY"
+
+// console.log('Bem vindo ' + nickname)
+
+
+// Name,old,number of telhephone,cpf,endereço
+
+
+// name = string
+// old = number
+// numbercell = string
+// cpf = string
+// endereco = string
