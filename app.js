403bceb91c0bac9df67681eff566011cd6162442
julio gabriel <juliogabriel78956@gmail.com>
2025-11-23 18:01:05 -0300

    Estudo

--- app.js
+++ app.js
@@ -16,3 +16,62 @@
 // numbercell = string
 // cpf = string
 // endereco = string
+
+
+
+// OPERADORES MATEMATICOS
+
+// let idade = 17
+
+// console.log( ' Value of my variable'  + idade)
+
+// idade = 17 / 4
+// console.log(idade)
+
+
+// Incremento e decremento
+
+// let contador = 1 
+//  incremento sempre vai adicionar mais um ao numero
+//  contador++
+//  contador++
+
+//  console.log(contador)
+
+
+// deceremento
+// let contador = 13
+
+// contador--
+// contador--
+// contador--
+// contador--
+// console.log(contador)
+
+
+
+// Calcular  e atribuir
+// let preco = 10
+// preco +=  5
+
+// let preco = 30
+// preco -= 40
+
+// console.log(preco) 
+
+
+
+// Crie uma expressão em JavaScript que some dois números e depois multiplique o resultado por 3. Qual será o valor final?
+
+// let number1 = 200
+// let number2 = 300
+
+// let total = number1 + number2 
+// console.log(total *3)
+
+
+// 2. No JavaScript, qual seria o resultado da divisão de 25 por 4? Explique por que o número não é inteiro. 
+
+// let conta =  25 / 4 
+
+// console.log(conta)
